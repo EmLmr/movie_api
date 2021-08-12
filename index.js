f329975a23870086a0ccd06a15a78ecdd9118ab8
EmLmr <emeline.coding@gmail.com>
2021-08-12 11:55:13 +0200

    connected app to remote DB

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+mongoose.connect('process.env.CONNECTION_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
