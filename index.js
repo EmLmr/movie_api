45cd30a5c594a397dfc10cdf39f61245982124ee
EmLmr <86126621+EmLmr@users.noreply.github.com>
2021-11-22 10:55:06 +0100

    added all github deployment links

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ let allowedOrigins = [
     'http://localhost:1234',
     'https://flickspicks.netlify.app',
     'http://localhost:4200',
+    'https://emlmr.github.io/FlicksPicks-Angular-client/',
+    'https://emlmr.github.io/',
+    'https://emlmr.github.io',
     'https://emlmr.github.io/FlicksPicks-Angular-client/'
+    'https://emlmr.github.io/FlicksPicks-Angular-client',
+    
+  
 ];
 
 app.use(
